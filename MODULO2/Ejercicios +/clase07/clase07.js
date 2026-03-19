e17638f9ebf2509ca60aeced75a32cd495acd758
Lala <alejomaidana29@gmail.com>
2026-03-18 21:26:39 -0300

    clase consulta 03

--- MODULO2/Ejercicios +/clase07/clase07.js
+++ MODULO2/Ejercicios +/clase07/clase07.js
@@ -69,30 +69,3 @@ Consideraciones: para realizar el ejercicio debe utilizar solamente código Java
     console.log(`El porcentaje de huevos Descartados es: ${huevosPorcentajeDescarte}`);
 
 }
-
-
-/* 
-
-Ejercicio Nro. 18:
-    Un supermercado desea realizar una aplicación WEB que le permita a sus cajeros realizar los cobros a los clientes que atenderán en su jornada laboral. La aplicación debe tener las siguientes consideraciones.
-    -	Un cajero podrá atender a “n” clientes, siendo n el número que no está definido, puede atender a 0, 2, 5 clientes. 
-    Nota: debería resolverse aplicando un ciclo while de clientes
-
-    -	Cada cliente atendido llevará “m” productos, siendo m el número que no está definido. Puede llevar 10, 15, 20, 50 productos. 
-    Nota: debería resolverse aplicando un ciclo while de productos.
-
-    -	Al finalizar la atención del cliente, debería preguntar si paga con:
-        o	1 – Efectivo – aplicar descuento 10%
-        o	2 – Tarjeta Visa BNA – aplicar descuento 7%
-        o	3 – Tarjeta Master BNA – aplicar descuento 7%
-        o	4 – Otras Tarjetas Medios – sin descuentos.
-    
-    -	Al finalizar la atención del cliente deberá ir registrando los siguientes puntos
-        o	Venta Total = Acumular la venta de todos los clientes
-        o	Descuento Total = Acumular los descuentos realizados a cada cliente
-        o	Importe a Pagar Total = Acumular los Importes a pagar por cada cliente
-        o	Contar la cantidad de clientes que Tuvieron descuentos
-        o	Contar la cantidad de clientes que No Tuvieron descuentos.
-
-    Nota: Este proceso debe confeccionarse con dos ciclos while, un ciclo while atenderá a los clientes y el otro ciclo while (interno) registrará los productos que lleva cada cliente.
-*/
