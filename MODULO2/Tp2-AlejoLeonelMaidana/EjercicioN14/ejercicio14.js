c5cac52c3ef057f4270c1cce140421491995fde2
Lal4-devMini <alejomaidana29@gmail.com>
2026-03-29 02:27:56 -0300

    inicio tp 2

---
+++ MODULO2/Tp2-AlejoLeonelMaidana/EjercicioN14/ejercicio14.js
@@ -0,0 +1,42 @@
+/*
+Ejercicio Nro. 14: 
+ 
+Una  empresa  de  Peajes  del  SUR  S.A.  desea  realizar  una  aplicación  que  permita  cobrar  y  otorgar  los 
+comprobantes a los clientes que circulan por las autopistas concesionadas por la empresa, para ello se necesita 
+realizar un programa que el cajero del puesto pueda realizar el cobro y levantado de la barrera. 
+ 
+Los precios establecidos son los siguientes 
+ 
++ CAMION = 22.000,00 PESOS 
++ CAMIONETA = 12.000,00 PESOS 
++ AUTOMOVIL = 4.000,00 PESOS 
++ MOTOS = 1.500,00 PESOS 
+ 
+La aplicación debe permitirle al cajero registrar todas las operaciones y al finalizar su jornada laboral realizar el 
+cierre donde le deberá mostrar la siguiente información. 
+ 
++ Cantidad de AUTOS  
++ Total Cobrado de AUTOS 
+ 
++ Cantidad de CAMIONETAS 
++ Total de CAMIONETAS 
+ 
++ Cantidad de AUTOMOVILES 
++ Total de Automóviles 
+ 
++ Cantidad de MOTOS 
++ Total de MOTOS 
+ 
++ Total GENERAL cobrado. 
+ 
+Compare al final si pasaron más camionetas que autos o fue al revés ¿?. O quizás hayan pasado la misma 
+cantidad. Compare estas cantidades únicamente entre autos y camionetas. 
+ 
+Consideraciones: para realizar el ejercicio debe utilizar solamente código JavaScript, sin interacción con el 
+DOM y cargar los datos de entrada por medio de prompt. 
+
+*/
+
+{
+    
+}
