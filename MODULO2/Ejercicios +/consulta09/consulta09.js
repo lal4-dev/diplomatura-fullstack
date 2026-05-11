636f064a68f39198a29e5316b7f22a97ad208b12
Lal4-devMini <alejomaidana29@gmail.com>
2026-05-11 17:26:57 -0300

    consulta9-metodos-array

--- MODULO2/Ejercicios +/consulta09/consulta09.js
+++ MODULO2/Ejercicios +/consulta09/consulta09.js
@@ -1,4 +1,6 @@
-import {productos} from "./modelo.js"
+import {productos} from "./modelo.js";
+
+let sueldos = [32000,17000,78000,10000];
 window.onload = () =>{
     console.log(productos);
 
@@ -13,4 +15,28 @@ window.onload = () =>{
             console.log(producto);
         }
     });
-}
+
+    productos.forEach(productos => productos.precio <= 20000? console.log(productos):``);
+
+    let productoEncontrado = productos.find(productos=>{
+        return productos.id === 6 ? true:false;
+    });
+
+    console.log(productoEncontrado);
+
+
+    let productoEncontrado2 = productos.find(producto => producto.id ===6);
+    console.log(productoEncontrado2);
+
+    let calzados = productos.filter(producto => producto.categoria === `calzado`);
+    console.log(calzados);
+
+    let resultado = productos.some(producto=> producto.stock===0);
+    console.log(resultado);
+
+    let resultado2 = productos.every(producto=> producto.stock >= 5);
+    console.log(resultado2)
+
+    let quincesas = sueldos.map(sueldos => sueldos/2);
+    console.log(quincesas);
+}   
